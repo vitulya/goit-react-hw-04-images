dce61ac77efd961b1f2e864080438f474e472467
Vitulya <vitulya.bondarchuk@gmail.com>
2023-03-03 21:40:07 +0200

    stuk

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,22 +1,16 @@
 import css from './ImageGallery.module.css';
 import { ImageGalleryItem } from '../ImageGalleryItem/ImageGalleryItem.jsx';
-import { Component } from 'react';
 import PropTypes from 'prop-types';
 
-export class ImageGallery extends Component {
-  render() {
-    return (
-      <>
-        <ul className={css.ImageGallery}>
-          <ImageGalleryItem
-            onOpenModal={this.props.onOpenModal}
-            images={this.props.images}
-          />
-        </ul>
-      </>
-    );
-  }
-}
+export const ImageGallery = ({ onOpenModal, images }) => {
+  return (
+    <>
+      <ul className={css.ImageGallery}>
+        <ImageGalleryItem onOpenModal={onOpenModal} images={images} />
+      </ul>
+    </>
+  );
+};
 
 ImageGallery.propTypes = {
   images: PropTypes.arrayOf(PropTypes.shape()),
