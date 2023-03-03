56ca6410f401a30818a44b6993f1e01ee64e86f7
Vitulya <vitulya.bondarchuk@gmail.com>
2023-03-03 21:49:55 +0200

    update modal

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,41 +1,76 @@
-import { Component } from 'react';
+// import { Component } from 'react';
 import css from './Modal.module.css';
 import PropTypes from 'prop-types';
+import { useEffect } from 'react';
 
-export class Modal extends Component {
-  componentDidMount() {
-    window.addEventListener('keydown', this.handleEscPress);
-  }
+export const Modal = ({ onCloseModal, image }) => {
+  useEffect(() => {
+    const handleEscPress = event => {
+      if (event.key === 'Escape') {
+        onCloseModal();
+      }
+    };
 
-  componentWillUnmount() {
-    window.removeEventListener('keydown', this.handleEscPress);
-  }
+    window.addEventListener('keydown', handleEscPress);
 
-  handleBackdropClick = event => {
+    return () => {
+      window.removeEventListener('keydown', handleEscPress);
+    };
+  }, [onCloseModal]);
+
+  const handleBackdropClick = event => {
     if (event.currentTarget === event.target) {
-      this.props.onCloseModal();
+      onCloseModal();
     }
   };
 
-  handleEscPress = event => {
-    if (event.key === 'Escape') {
-      this.props.onCloseModal();
-    }
-  };
-
-  render() {
-    return (
-      <>
-        <div onClick={this.handleBackdropClick} className={css.Overlay}>
-          <div className={css.Modal}>
-            <img src={this.props.image.largeImageURL} alt="" />
-          </div>
+  return (
+    <>
+      <div onClick={handleBackdropClick} className={css.Overlay}>
+        <div className={css.Modal}>
+          <img src={image.largeImageURL} alt="" />
         </div>
-        ;
-      </>
-    );
-  }
-}
+      </div>
+      ;
+    </>
+  );
+};
+
+// export class Modal extends Component {
+
+//   componentDidMount() {
+//     window.addEventListener('keydown', this.handleEscPress);
+//   }
+
+//   componentWillUnmount() {
+//     window.removeEventListener('keydown', this.handleEscPress);
+//   }
+
+//   handleBackdropClick = event => {
+//     if (event.currentTarget === event.target) {
+//       this.props.onCloseModal();
+//     }
+//   };
+
+//   handleEscPress = event => {
+//     if (event.key === 'Escape') {
+//       this.props.onCloseModal();
+//     }
+//   };
+
+//   render() {
+//     return (
+//       <>
+//         <div onClick={this.handleBackdropClick} className={css.Overlay}>
+//           <div className={css.Modal}>
+//             <img src={this.props.image.largeImageURL} alt="" />
+//           </div>
+//         </div>
+//         ;
+//       </>
+//     );
+//   }
+// }
 
 Modal.propTypes = {
   image: PropTypes.shape({
