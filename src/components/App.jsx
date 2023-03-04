4cde012540a06b4d977c4741871c35a1edb30c6d
Vitulya <vitulya.bondarchuk@gmail.com>
2023-03-04 22:45:52 +0200

    Update App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -56,7 +56,7 @@ export const App = () => {
 
       if (page !== 1) {
         try {
-          const fetchImages = await getImages(page, value);
+          const fetchImages = await getImages(page, submitValue);
           setImages(prevImage => [...prevImage, ...fetchImages.hits]);
         } catch (error) {
           setError(error);
@@ -65,7 +65,7 @@ export const App = () => {
         }
       } else {
         try {
-          const images = await getImages(page, value);
+          const images = await getImages(page, submitValue);
           setPage(1);
           setImages(images.hits);
           setTotalHits(images.totalHits);
@@ -79,56 +79,15 @@ export const App = () => {
 
     if (submitValue) {
       fetchImages();
+    } else {
+      setImages(null);
+      setPage(1);
+      setTotalHits(null);
+      return;
     }
+  }, [page, submitValue]);
 
-    // async function foo() {
-    //   try {
-    //     const images = await getImages(page, value);
-    //     console.log(images);
-    //     setImages(images.hits);
-    //     // setImages(prevImage => [...prevImage, ...images.hits]);
-    //   } catch (error) {
-    //     setError(error);
-    //   } finally {
-    //     setIsLoading(false);
-    //   }
-    // }
-    // if (submitValue) {
-    //   foo();
-    // }
-  }, [page, submitValue, value]);
 
-  // useEffect(() => {
-  //   async function foo() {
-  //     setIsLoading(true);
-  //     try {
-  //       const images = await getImages(page, value);
-  //       setImages(prevImage => [...prevImage, ...images.hits]);
-  //     } catch (error) {
-  //       setError(error);
-  //     } finally {
-  //       setIsLoading(false);
-  //     }
-  //   }
-  //   foo();
-  // }, [page, value]);
-
-  // useEffect(() => {
-  // async function foo() {
-  //   setIsLoading(true);
-  //   try {
-  //     const images = await getImages(page, value);
-  //     setPage(1);
-  //     setImages(images.hits);
-  //     setTotalHits(images.totalHits);
-  //   } catch (error) {
-  //     setError(error);
-  //   } finally {
-  //     setIsLoading(false);
-  //   }
-  // }
-  // foo();
-  // }, [page, value, submitValue]);
 
   const handleChangePage = () => {
     setPage(prevPage => prevPage + 1);
@@ -172,115 +131,3 @@ export const App = () => {
   );
 };
 
-// export class App extends Component {
-//   state = {
-//     images: null,
-//     page: 1,
-//     value: '',
-//     submitValue: '',
-//     isOpenModal: false,
-//     currentImage: null,
-//     isLoading: false,
-//     error: null,
-//     totalHits: null,
-//   };
-
-//   async componentDidUpdate(_, prevState) {
-//     const { page, value, submitValue } = this.state;
-
-//     if (prevState.page !== page) {
-//       this.setState({
-//         isLoading: true,
-//       });
-//       try {
-//         const images = await getImages(page, value);
-//         this.setState(prev => ({
-//           images: [...prev.images, ...images.hits],
-//         }));
-//       } catch (error) {
-//         this.setState({
-//           error,
-//         });
-//       } finally {
-//         this.setState({
-//           isLoading: false,
-//         });
-//       }
-//     }
-
-//     if (prevState.submitValue !== submitValue) {
-//       this.setState({
-//         isLoading: true,
-//       });
-//       try {
-//         const images = await getImages(page, value);
-//         console.log(images);
-//         this.setState({
-//           page: 1,
-//           images: images.hits,
-//           totalHits: images.totalHits,
-//         });
-//       } catch (error) {
-//         this.setState({
-//           error,
-//         });
-//       } finally {
-//         this.setState({
-//           isLoading: false,
-//         });
-//       }
-//     }
-//   }
-
-//   handleChangePage = () => {
-//     this.setState(prev => ({ page: prev.page + 1 }));
-//   };
-
-//   handleChangeValue = e => {
-//     const { value } = e.target;
-
-//     this.setState({
-//       value,
-//     });
-//   };
-
-//   handleSubmitInputValue = e => {
-//     e.preventDefault();
-//     this.setState({
-//       submitValue: this.state.value,
-//     });
-//   };
-
-//   handleToggleModal = image => {
-//     this.setState(prevState => ({
-//       isOpenModal: !prevState.isOpenModal,
-//       currentImage: image,
-//     }));
-//   };
-
-//   render() {
-//     const { currentImage, isLoading, error, images, totalHits } = this.state;
-//     return (
-//       <div className={css.App}>
-//         <Searchbar
-//           handleChangeValue={this.handleChangeValue}
-//           onSubmit={this.handleSubmitInputValue}
-//         />
-
-//         <ImageGallery
-//           onOpenModal={this.handleToggleModal}
-//           images={this.state.images}
-//           isLoading={isLoading}
-//         />
-//         {isLoading && <Loader />}
-//         {error && <h1>Спробуйте перезагрузити сторінку...</h1>}
-//         {images?.length < totalHits && (
-//           <Button onClick={this.handleChangePage} />
-//         )}
-//         {this.state.isOpenModal && (
-//           <Modal onCloseModal={this.handleToggleModal} image={currentImage} />
-//         )}
-//       </div>
-//     );
-//   }
-// }
